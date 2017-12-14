30b67c0a5b0a8af5b42a0e3f422f3f7543b218ad
fengmengchuan <631606325@qq.com>
2017-12-14 19:01:57 +0800

    modify addcommitpush.sh

--- addcommitpush.sh
+++ addcommitpush.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 git add .
 #add ""
-<<<<<<< HEAD
+
 git commit -m "20171214 acer nothing"
-=======
+
 git commit -m "20171214 nothing"
->>>>>>> b97da8075ffe41cd31862734b472b4c6423fd171
+
 git push
