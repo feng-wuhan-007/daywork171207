d78f79468b9a87f88cddafee7f98eebcc824d846
fengmengchuan <631606325@qq.com>
2018-04-08 17:31:08 +0800

    modify cpluscode

--- cpluscode/helloworld/helloworld.cpp
+++ cpluscode/helloworld/helloworld.cpp
@@ -1,9 +1,22 @@
-#include <iostream>
-using namespace std;
-class A{
-};
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
 int main()
 {
-  cout<<"Hello World\n";
+  pid_t child_pid;
+  int count = 0;
+//create a child pid
+child_pid = fork();
+if(child_pid == 0){
+	printf("child pid\n");
+        count++;
+	exit(0);
+}else{
+printf("father pid\n");
+count++;
+sleep(2);
+}
+  printf("count=%d\n",count);
   return 0;
 }
